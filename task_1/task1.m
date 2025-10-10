ba349282e4c7459be9c869a478a777c4c3049bfb
Yurii Tyrpych <yurii.tyrpych@cuebric.com>
2025-10-10 09:34:46 +0300

    feat: add task 1

---
+++ task_1/task1.m
@@ -0,0 +1,9 @@
+format short e 
+
+a = 1.7;
+b = -1.25;
+c = -0.3;
+x = 2.5;
+k = 3;
+
+y = sqrt( a * b * c / 2.4 ) - ( 0.7 * a * b * c) / sin(7) + 10^4 * abs( cos( k * x) )^(1/5) - (abs( b - a) ) / k * x
